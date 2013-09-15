8938ddd275f82585f4624e43f8abbd7e1f59da5b
Jesse Lin <jesselin@gmail.com>
2013-09-15 16:50:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsCv::Application.config.secret_key_base = '43a8058c4776f072287033365f3fe96b884c4955beb90190c5ef951acd31b866445ac9702ee4fd6ea1217c8127b6aab9495747395ca34403d1192ca144fb2616'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsCv::Application.config.session_store :cookie_store, key: '_rails-cv_session'
